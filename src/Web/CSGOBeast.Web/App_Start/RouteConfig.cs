f542d25d90ca4f08552a81b8f74f191984f70332
radenkovn <radenkovn@yahoo.com>
2016-02-24 10:49:47 +0200

    Added Sell Option

--- src/Web/CSGOBeast.Web/App_Start/RouteConfig.cs
+++ src/Web/CSGOBeast.Web/App_Start/RouteConfig.cs
@@ -9,9 +9,9 @@
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.MapRoute(
-                name: "JokePage",
-                url: "Joke/{id}",
-                defaults: new { controller = "Jokes", action = "ById" });
+                name: "Inventory",
+                url: "Inventory",
+                defaults: new { controller = "Inventory", action = "Inventory" });
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- src/Web/CSGOBeast.Web/Controllers/InventoryController.cs
+++ src/Web/CSGOBeast.Web/Controllers/InventoryController.cs
@@ -5,10 +5,12 @@
     using System.Linq;
     using System.Web;
     using System.Web.Mvc;
+    using Common;
     using CSGOBeast.Services.Data;
     using Microsoft.AspNet.Identity;
+    using ViewModels.Inventory;
     using ViewModels.Items;
-    using Common;
+
     [Authorize]
     public class InventoryController : BaseController
     {
@@ -21,6 +23,15 @@
             this.users = users;
         }
 
+        public ActionResult Inventory()
+        {
+            var userId = this.User.Identity.GetUserId();
+            var user = this.users.GetById(userId);
+            var viewModel = this.Mapper.Map<UserInventoryViewModel>(user);
+
+            return this.View(viewModel);
+        }
+
         public ActionResult BuyItem(BuyModel item)
         {
             if (!this.ModelState.IsValid)
@@ -30,11 +41,27 @@
 
             var userId = this.User.Identity.GetUserId();
 
-            this.users.Buy(userId, item.ItemId);
+            var user = this.users.Buy(userId, item.ItemId);
+            this.Cache.Remove(GlobalConstants.ItemsInfoCache);
+
+            var viewModel = this.Mapper.Map<UserInventoryViewModel>(user);
+
+            return this.Redirect("/Inventory/");
+        }
+
+        public ActionResult SellItem(SellModel item)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.Redirect("/Shop/Items");
+            }
+
+            var userId = this.User.Identity.GetUserId();
+            var user = this.users.Sell(userId, item.ItemId);
 
             this.Cache.Remove(GlobalConstants.ItemsInfoCache);
 
-            return this.View();
+            return this.Redirect("/Inventory/");
         }
     }
 }
