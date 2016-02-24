a959139a943316f1d92d97c6ea8c1e0da70f9f73
radenkovn <radenkovn@yahoo.com>
2016-02-24 12:26:11 +0200

    Added Errors Controller

---
+++ src/Web/CSGOBeast.Web/Controllers/ErrorsController.cs
@@ -0,0 +1,16 @@
+﻿namespace CSGOBeast.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class ErrorsController : BaseController
+    {
+        public ActionResult Error()
+        {
+            return this.View();
+        }
+    }
+}
