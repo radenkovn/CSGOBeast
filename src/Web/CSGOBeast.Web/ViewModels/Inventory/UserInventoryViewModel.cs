4201f480c39b90c3bdfe454f9e5a990aa80f232d
radenkovn <radenkovn@yahoo.com>
2016-02-24 10:50:21 +0200

    Added UserInventoryViewModel

---
+++ src/Web/CSGOBeast.Web/ViewModels/Inventory/UserInventoryViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace CSGOBeast.Web.ViewModels.Inventory
+{
+    using System.Collections.Generic;
+    using CSGOBeast.Data.Models;
+    using CSGOBeast.Web.Infrastructure.Mapping;
+    using Items;
+
+    public class UserInventoryViewModel : IMapFrom<User>
+    {
+        public decimal Balance { get; set; }
+
+        public IEnumerable<ItemDetailsViewModel> Items { get; set; }
+
+    }
+}
