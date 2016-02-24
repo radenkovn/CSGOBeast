34f385ebc1d6a9fff21622d63ed625d7ee7f3315
radenkovn <radenkovn@yahoo.com>
2016-02-24 11:38:22 +0200

    Added Coinflip Service

---
+++ src/Services/CSGOBeast.Services.Data/ICoinflipsService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSGOBeast.Services.Data
+{
+    public interface ICoinFlipsService
+    {
+    }
+}
